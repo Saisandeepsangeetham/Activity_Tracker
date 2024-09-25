ec36ab9a93975ffa75cc5f6e59e094219abef01e
Saisandeep Sangeetham <134389218+Saisandeepsangeetham@users.noreply.github.com>
2024-09-25 18:50:06 +0530

    Handled cookies

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './Routes/index.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 //morgan for developing...
 app.use(morgan("dev"));
 
